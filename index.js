cf94b3e42737b2b9011bad2de1bc6e1a0ecccf78
Abhilashghope <33655535+Abhilashghope@users.noreply.github.com>
2025-06-20 11:49:37 +0530

    Revert "notfication changes (#16)" (#18)
    This reverts commit f9ccedc08c0f909d1ab91bcaa5b71c19c103262d.

--- index.js
+++ index.js
@@ -3,45 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import {AndroidBadgeIconType} from '@notifee/react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import messaging from '@react-native-firebase/messaging';
-import notifee from '@notifee/react-native';
-
-messaging().setBackgroundMessageHandler(async remoteMessage => {
-  if (!remoteMessage) {
-    return;
-  }
-
-  if (remoteMessage.data && remoteMessage.data.customPayload) {
-    const parsedPayload = JSON.parse(remoteMessage?.data?.customPayload);
-
-    console.log(
-      '[notificationnn]Background notification received:',
-      remoteMessage.data,
-    );
-    // const msg = `New order received:ID ${parsedPayload?.orderId}, Name:${parsedPayload?.customerName} Amount: ${parsedPayload?.amount}`;
-    const msg = `New order received: From ${parsedPayload?.customerName}, Amount: ₹${parsedPayload?.amount}`;
-    console.log('[notificationnn] Parsed Payload:', parsedPayload);
-    console.log('[notificationnn] msg:', msg);
-    await notifee.displayNotification({
-      title: remoteMessage?.data?.title,
-      body: msg,
-      android: {
-        channelId: 'sound_channel',
-        sound: 'noti',
-        smallIcon: 'qv_blue',
-        badgeIconType: AndroidBadgeIconType.SMALL,
-        color: '#8B8000',
-        vibrationPattern: [300, 500],
-        showTimestamp: true,
-        pressAction: {
-          id: 'default',
-        },
-      },
-    });
-  }
-});
 
 AppRegistry.registerComponent(appName, () => App);
